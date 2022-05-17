22b71d8bf861df13c1a3a6cabd5f4f059085e229
claudemyburgh <claudemyburgh@gmail.com>
2022-05-17 16:50:24 +0200

    wip

--- components/navigation/NavMain.js
+++ components/navigation/NavMain.js
@@ -25,11 +25,13 @@ const links = [
 
 export default function NavMain() {
   return (
-    <nav className={`my-10 flex space-x-2 rounded-sm border border-primary-500 bg-gray-900 p-2.5 shadow-xl shadow-primary-500/10`}>
+    <nav
+      className={`my-10 flex space-x-2 overflow-x-auto rounded-sm border border-primary-500 bg-gray-900 p-2.5 shadow-xl shadow-primary-500/10`}
+    >
       {links.map((link) => (
         <Link key={link.link} href={link.link}>
           <a
-            className={`rounded-sm bg-primary-500/10 bg-gradient-to-b px-6 py-1.5 font-bold text-primary-500 transition duration-100 hover:bg-primary-500 hover:text-gray-900`}
+            className={`shrink-0 rounded-sm bg-primary-500/10 bg-gradient-to-b px-6 py-1.5 font-bold text-primary-500 transition duration-100 hover:bg-primary-500 hover:text-gray-900`}
           >
             {link.label}
           </a>
